056accf526cfcebbff6dc9982299ec2bd99e28db
yasirupanduka99 <22panduka@gmail.com>
2024-05-19 16:11:14 +0530

    Math Operator Short-hand in JS ✨🫡

---
+++ 06_JavaScript - JS Math Operator Short-hand/script.js
@@ -0,0 +1,33 @@
+alert("JS Math Operator Short-hand!");
+
+/*
+    JavaScript Assignment Operators
+    Assignment operators assign values to JavaScript variables.
+    The Addition Assignment Operator (+=) adds a value to a variable.
+
+    let x = 10;
+    x += 5;
+
+    Operator	Example	    Same As
+    =	        x = y	    x = y
+    +=	        x += y	    x = x + y
+    -=	        x -= y	    x = x - y
+    *=	        x *= y	    x = x * y
+    /=	        x /= y	    x = x / y
+    %=	        x %= y	    x = x % y
+    **=	        x **= y	    x = x ** y
+
+
+    JavaScript Comparison Operators
+
+    Operator	Description
+    ==	        equal to
+    ===	        equal value and equal type
+    !=	        not equal
+    !==	        not equal value or not equal type
+    >	        greater than
+    <	        less than
+    >=	        greater than or equal to
+    <=	        less than or equal to
+    ?	        ternary operator
+*/
