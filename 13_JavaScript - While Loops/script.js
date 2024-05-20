1184b37b6f8d23ecc5d2b639ecd58cd9077404ba
yasirupanduka99 <22panduka@gmail.com>
2024-05-20 18:35:50 +0530

    While Loops in JavaScript 😁✨

---
+++ 13_JavaScript - While Loops/script.js
@@ -0,0 +1,24 @@
+alert("While Loops in JS!");
+
+/*
+
+    The While Loop
+    The while loop loops through a block of code as long as a specified condition is true.
+
+    Syntax
+    while (condition) {
+    // code block to be executed
+    }
+
+*/
+
+let age = 5;
+
+document.write("See browser console to print while loop inside statement!");
+
+while (age < 10) {
+    console.log("Your age is less than 10 🔞");
+    age++;
+}
+
+document.write("<br />You are now over 10");
