4b79d8c88d2761d9c0e7f0419ac570b6ee6cea52
yasirupanduka99 <22panduka@gmail.com>
2024-05-31 23:43:24 +0530

    What is DOM in JS ✨🍾

---
+++ 29_JavaScript - What is the DOM/script.js
@@ -0,0 +1,88 @@
+document.write("What is DOM in JavaScript!" + "<br /><br />");
+
+/*
+    JavaScript HTML DOM
+    
+        With the HTML DOM, JavaScript can access and change all the elements of an HTML document.
+
+
+    The HTML DOM (Document Object Model)
+
+        - When a web page is loaded, the browser creates a Document Object Model of the page.
+        - The HTML DOM model is constructed as a tree of Objects:
+
+        The HTML DOM Tree of Objects
+        DOM HTML tree Image : https://www.w3schools.com/js/pic_htmltree.gif
+        
+        With the object model, JavaScript gets all the power it needs to create dynamic HTML:
+
+            - JavaScript can change all the HTML elements in the page
+            - JavaScript can change all the HTML attributes in the page
+            - JavaScript can change all the CSS styles in the page
+            - JavaScript can remove existing HTML elements and attributes
+            - JavaScript can add new HTML elements and attributes
+            - JavaScript can react to all existing HTML events in the page
+            - JavaScript can create new HTML events in the page
+
+
+    What is the DOM?
+        The DOM is a W3C (World Wide Web Consortium) standard.
+
+        The DOM defines a standard for accessing documents:
+
+        "The W3C Document Object Model (DOM) is a platform and language-neutral interface that allows programs and scripts to dynamically access and update the content, structure, and style of a document."
+
+        The W3C DOM standard is separated into 3 different parts:
+
+            - Core DOM - standard model for all document types
+            - XML DOM - standard model for XML documents
+            - HTML DOM - standard model for HTML documents
+
+
+
+    What is the HTML DOM (W3School)?
+
+        The HTML DOM is a standard object model and programming interface for HTML. It defines:
+
+            - The HTML elements as objects
+            - The properties of all HTML elements
+            - The methods to access all HTML elements
+            - The events for all HTML elements
+
+        In other words: The HTML DOM is a standard for how to get, change, add, or delete HTML elements.
+
+    
+
+    What is the HTML DOM (Document Object Model) (Net Ninja)?
+
+        - The DOM is an 'application programming interface'
+        - Use the DOM when we interact with web pages
+
+                + Add content to a HTML document
+                + Delete content from a HTML document 
+                + Change content on a HTML document
+
+                Note : The document is just the web page
+
+        - Every HTML element in the document is an object:
+                + <head></head> is an object
+                + <body></body> is an object
+                + <ul></ul> is an object
+                + <p> is an object
+                + etc... (Every tags in HTML document are objects)
+
+        - NODES in HTML
+                + Everything we can change in the document is a node:
+                    ++ Elements
+                    ++ Text within elements
+                    ++ HTML attributes
+
+        - What can we do with the DOM?
+                + Change the <h1> text node at the top of the page
+                + Change the background colour of an element node
+                + Animate the logo node from left to right
+                + Expand the height of an element node when you click on it
+                + PLUS MUCH MORE...
+
+*/
+
